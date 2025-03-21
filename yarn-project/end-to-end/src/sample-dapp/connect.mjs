1a78fc5fdf566131c4e1ba57e65eb5a48f027d51
megabyte <megabyte0x@gmail.com>
2025-03-21 21:02:16 +0530

    fix: await for client creation

--- yarn-project/end-to-end/src/sample-dapp/connect.mjs
+++ yarn-project/end-to-end/src/sample-dapp/connect.mjs
@@ -4,7 +4,8 @@ import { createPXEClient } from '@aztec/aztec.js';
 const { PXE_URL = 'http://localhost:8080' } = process.env;
 
 async function main() {
-  const pxe = await createPXEClient(PXE_URL);
+  const pxe = createPXEClient(PXE_URL);
+  await waitForPXE(pxe);
   const { l1ChainId } = await pxe.getNodeInfo();
   console.log(`Connected to chain ${l1ChainId}`);
 }
